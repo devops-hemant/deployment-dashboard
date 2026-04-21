4774042c40b655cad8e5934ad8dcc7fcddd73b17
devops-hemant <devopshemant@gmail.com>
2026-04-22 01:18:46 +0530

    udpate2

--- api/GetBlobs/index.js
+++ api/GetBlobs/index.js
@@ -9,10 +9,17 @@ module.exports = async function (context, req) {
     }
 
     try {
+        // 1. Create the client using the full URL
         const blobServiceClient = new BlobServiceClient(SAS_URL);
         
-        // This regex extracts the container name even if it's $web
-        const containerName = SAS_URL.split('.net/')[1].split('?')[0];
+        // 2. Manually define the container name if the URL parsing is failing
+        // Or, use this more robust parsing logic:
+        const urlObj = new URL(SAS_URL);
+        // This takes the part between '.net/' and the '?'
+        const containerName = urlObj.pathname.split('/').filter(Boolean)[0];
+
+        context.log(`Targeting container: ${containerName}`);
+
         const containerClient = blobServiceClient.getContainerClient(containerName);
 
         let blobs = [];
@@ -26,6 +33,10 @@ module.exports = async function (context, req) {
             body: blobs
         };
     } catch (error) {
-        context.res = { status: 500, body: `Storage Error: ${error.message}` };
+        // This will now report the specific container it tried to find
+        context.res = { 
+            status: 500, 
+            body: `Storage Error: ${error.message} (Attempted Container: ${SAS_URL.split('.net/')[1]?.split('?')[0]})` 
+        };
     }
 };
